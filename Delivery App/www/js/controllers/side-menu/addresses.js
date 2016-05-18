7acf08c8eee1c1ca3b74a3c3efbc1d78ef84f6c3
Yaser El Sabbouh <yaser.alsabbouh@gmail.com>
2016-05-18 12:42:10 +0300

    primitive commit to Addresses screens

--- Delivery App/www/js/controllers/side-menu/addresses.js
+++ Delivery App/www/js/controllers/side-menu/addresses.js
@@ -1,10 +1,24 @@
 angular.module('delivery.controllers')
 
-.controller('AddressesCtrl', function ($scope, $rootScope, $ionicLoading, $timeout) {
+.controller('AddressesCtrl', function ($scope, $rootScope, $ionicLoading, $timeout, $ionicModal) {
 
     /// <summary>closeAddresses: Close the addresses modal when user press back</summary>
     /// <param>No parameters</param>
     $scope.close = function () {
         $rootScope.addressesModal.hide();
+    };
+
+    $ionicModal.fromTemplateUrl('editAddress-modal.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.addressEditModal = modal;
+    });
+
+    $scope.showAddressModal = function () {
+        $scope.addressEditModal.show();
+    };
+
+    $scope.closeAddressModal = function () {
+        $scope.addressEditModal.hide();
     };
 });
