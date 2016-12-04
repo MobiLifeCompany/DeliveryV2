ec7023549da44c4a42b97bb91dd3ff561c1aba0a
“Nawar <nawar.a.albarazi@gmail.com>
2016-12-04 23:35:17 +0200

    Downgrade ionic version & add shop address to shops list

--- Delivery App/www/js/app.js
+++ Delivery App/www/js/app.js
@@ -94,7 +94,7 @@ angular.module('delivery', ['ionic', 'delivery.controllers', 'delivery.factory',
     $rootScope.opposite_float = $rootScope.lang === 'ar' ? 'left' : 'right';
 
     // for Syria the coutnry id = 1
-    $rootScope.versionCountry = 'lb';
+    $rootScope.versionCountry = 'sy';
     $rootScope.countryId = 1;
     $rootScope.baseURL = 'http://' + $rootScope.versionCountry + '.deliveryonweb.com/api/v1';
 
